ff4e5d43131c3daa671290b5c6bbd27ac6746977
pramijdhakle <pramij88@gmail.com>
2021-12-23 20:44:33 +0530

    done

--- springbootdockerapp/src/main/java/com/csi/controller/ApplicationController.java
+++ springbootdockerapp/src/main/java/com/csi/controller/ApplicationController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class ApplicationController {
-
+//code
     @GetMapping
     public String sayHello()
     {
